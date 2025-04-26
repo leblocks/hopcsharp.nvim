ad8fb57aa481f5969455391dc4cbf62911aa4c28
Sasha Gurevich <sagurevich@microsoft.com>
2025-04-26 12:54:57 +0300

    chore: added .init.lua for project local bindings

---
+++ .nvim.lua
@@ -0,0 +1,11 @@
+-- project local bindings and helper methods
+
+vim.api.nvim_set_keymap('n', '<F5>', ':so %<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F6>', ':PlenaryBustedFile %<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F9>', ':PlenaryBustedDirectory test/ <CR>', { noremap = true, silent = true })
+
+P = function(v)
+    print(vim.inspect(v))
+    return v
+end
+
