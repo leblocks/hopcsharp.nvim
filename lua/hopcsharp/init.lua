eb5c7295e8aef3e2b2e948fea4bffaaa9b3cd75a
Sasha Gurevich <sagurevich@microsoft.com>
2025-05-13 22:22:59 +0300

    feat: spawn parcing in a separate instance of neovim

--- lua/hopcsharp/init.lua
+++ lua/hopcsharp/init.lua
@@ -6,16 +6,11 @@ local database = require('hopcsharp.database')
 
 local M = {}
 
+local PROCESSING_ERROR_MESSAGE = 'init_database is running, try again later. If init_database failed - restart or manually set vim.g.hopcsharp_processing to false'
 vim.g.hopcsharp_processing = false
 
 local function log(message)
-    vim.notify('hopscsharp: ' .. message, vim.log.levels.INFO)
-end
-
-local function throw_on_processing()
-    if vim.g.hopcsharp_processing then
-        error('init_database is running, try again later. If init_database failed - restart or manually set vim.g.hopcsharp_processing to false')
-    end
+    print(message);
 end
 
 local function scheduled_iteration(i, iterable, callback)
@@ -28,51 +23,77 @@ local function scheduled_iteration(i, iterable, callback)
     vim.schedule(function() scheduled_iteration(i + 1, iterable, callback) end)
 end
 
-M.init_database = function()
-    throw_on_processing()
-
-    vim.g.hopcsharp_processing = true
-
+M.__init_database = function()
     -- drop existing schema
     database.__drop_db()
 
-    -- get files to process and calculate progress
-    local files = parse.__get_source_files()
-    local percent_step = 5
-    local step = #files / (100 / percent_step)
-    local total_percent_progress = 0
-    local counter = 0;
-
-    log('found ' .. #files .. ' files to process')
-
-    scheduled_iteration(1, files, function(i, items)
+    local counter = 0
+    scheduled_iteration(1, parse.__get_source_files(), function(i, items)
         parse.__parse_tree(items[i], function(tree, file_path, file_content, db)
             definition.__parse_definitions(tree:root(), file_path, file_content, db)
         end)
 
         counter = counter + 1
 
-        if counter >= step then
-            counter = 0
-            total_percent_progress = total_percent_progress + percent_step
-            log('processed ' .. total_percent_progress .. '% of source files')
-        end
-
-        if i == #items then
-            vim.g.hopcsharp_processing = false
-            log('finished processing source files')
+        if counter % 100 == 0 then
+            log(string.format('processed %s/%s of files', counter, #items))
         end
     end)
 end
 
+M.init_database = function()
+    if vim.g.hopcsharp_processing then
+        vim.notify(PROCESSING_ERROR_MESSAGE)
+        return
+    end
+
+    vim.g.hopcsharp_processing = true
+
+    local command = {
+        'nvim',
+        '--headless',
+        '-c', 'lua require("hopcsharp").__init_database()',
+        '-c', 'qa'
+    }
+
+    local on_stdout = function(_, data)
+        if #data > 0 then
+            print("hopcsharp: " .. table.concat(data, ''))
+        end
+    end
+
+    local on_exit = function(_)
+        vim.g.hopcsharp_processing = false
+        print('hopcsharp: finished processing files')
+        require 'plenary.profile'.stop()
+    end
+
+    require 'plenary.profile'.start("C:\\Users\\sagurevich\\hopcsharp.log")
+
+    -- spawn actual parsing in a separate instance of neovim
+    vim.fn.jobstart(command, {
+        cwd = vim.fn.getcwd(),
+        on_stdout = on_stdout,
+        on_exit = on_exit,
+        pty = true,
+        stdin = nil,
+    })
+end
+
 M.hop_to_definition = function(callback)
-    throw_on_processing()
+    if vim.g.hopcsharp_processing then
+        vim.notify(PROCESSING_ERROR_MESSAGE)
+        return
+    end
     hop.__hop_to_definition(callback)
 end
 
 ---@return sqlite_db
 M.get_db = function()
-    throw_on_processing()
+    if vim.g.hopcsharp_processing then
+        vim.notify(PROCESSING_ERROR_MESSAGE)
+        return
+    end
     return database.__get_db()
 end
 
