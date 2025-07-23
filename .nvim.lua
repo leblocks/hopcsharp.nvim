20b5e37b6a091344ae00625e19ccdb7add5ab8aa
Sasha Gurevich <sagurevich@microsoft.com>
2025-07-23 14:04:38 +0300

    chore: remove unneccessary files

--- .nvim.lua
+++
@@ -1,6 +0,0 @@
--- project local bindings and helper methods
-
-vim.api.nvim_set_keymap('n', '<F5>', ':so %<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F6>', ':PlenaryBustedFile %<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F9>', ':PlenaryBustedDirectory test/ { sequential = true } <CR>', { noremap = true, silent = true })
-
