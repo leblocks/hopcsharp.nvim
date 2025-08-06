b81c727221c3e2742924e78ec14cfb3ca9f0028c
Sasha Gurevich <sagurevich@microsoft.com>
2025-08-06 15:01:04 +0300

    feat: added fd check

--- lua/hopcsharp/health.lua
+++ lua/hopcsharp/health.lua
@@ -1,10 +1,18 @@
+local os = require('os')
+
 local M = {}
 
 local function check_fd()
+    vim.health.start('fd')
+    local exit_code = os.execute('fd --help')
+    if exit_code == 0 then
+        vim.health.ok('fd is on PATH')
+    else
+        vim.health.error('fd is not on PATH')
+    end
 end
 
-local function check_sqlite_installation()
-end
+local function check_sqlite_installation() end
 
 local function check_treesitter_c_sharp_grammar_installation()
     vim.health.start('treesitter')
