4386d5517228e926796efa46b24996459791b4f9
Sasha Gurevich <sagurevich@microsoft.com>
2026-02-13 23:30:17 +0200

    doc: added documentation for fzf pickers

--- lua/hopcsharp/pickers/fzf/init.lua
+++ lua/hopcsharp/pickers/fzf/init.lua
@@ -24,34 +24,25 @@ local get_items_by_type_picker = function(item_type)
     return utils.__get_picker(fzf, builtin, get_items_by_type(item_type), utils.__format_name_and_namespace)
 end
 
--- TODO add documentation in vimdoc
 M.source_files = file.__source_files(fzf)
 
--- TODO add documentation in vimdoc
 M.all_definitions = utils.__get_picker(fzf, builtin, function()
     local db = hopcsharp.get_db()
     return db:eval(db_query.get_all_definitions)
 end, utils.__format_name_and_namespace)
 
--- TODO add documentation in vimdoc
 M.class_definitions = get_items_by_type_picker(db_utils.types.CLASS)
 
--- TODO add documentation in vimdoc
 M.interface_definitions = get_items_by_type_picker(db_utils.types.INTERFACE)
 
--- TODO add documentation in vimdoc
 M.method_definitions = get_items_by_type_picker(db_utils.types.METHOD)
 
--- TODO add documentation in vimdoc
 M.struct_definitions = get_items_by_type_picker(db_utils.types.STRUCT)
 
--- TODO add documentation in vimdoc
 M.enum_definitions = get_items_by_type_picker(db_utils.types.ENUM)
 
--- TODO add documentation in vimdoc
 M.record_definitions = get_items_by_type_picker(db_utils.types.RECORD)
 
--- TODO add documentation in vimdoc
 M.attribute_definitions = utils.__get_picker(fzf, builtin, function()
     local db = hopcsharp.get_db()
     return db:eval(db_query.get_attributes)
