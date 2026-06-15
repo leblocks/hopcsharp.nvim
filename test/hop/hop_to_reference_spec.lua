3daba0fd4a5e35b7f94624691d85f4f50b10c0c6
Sasha Gurevich <sagurevich@microsoft.com>
2026-06-15 15:24:33 +0300

    test(reference): test become obsolete

--- test/hop/hop_to_reference_spec.lua
+++ test/hop/hop_to_reference_spec.lua
@@ -42,23 +42,6 @@ describe('hop_to_reference', function()
         assert(called)
     end)
 
-    it('__hop_to_reference hops from attribute usage to references', function()
-        utils.prepare('test/sources/hop_to_reference.cs', 'test/sources/hop_to_reference.cs', 11, 13)
-
-        hop.__hop_to_reference({ jump_on_quickfix = true })
-
-        local buf = vim.api.nvim_get_current_buf()
-        local name = vim.fs.normalize(vim.api.nvim_buf_get_name(buf))
-
-        assert(name:find('test/sources/hop_to_reference.cs$') ~= nil)
-        -- TODO now test is less valid, because we need to hop from its definition
-        --
-        local position = vim.fn.getcursorcharpos(0)
-        print(vim.inspect(position))
-        assert(position[2] == 5)
-        assert(position[3] == 10)
-    end)
-
     it('__hop_to_reference hops from class definition to references', function()
         utils.prepare('test/sources/hop_to_reference.cs', 'test/sources/Class2.cs', 5, 14)
 
